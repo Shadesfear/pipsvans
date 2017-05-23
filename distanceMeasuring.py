8225225ec07d5f4f8f899d836b2bd01c0d666f8a
Morten-Pc\Morten <mortendalfoss@gmail.com>
2017-05-23 14:35:56 +0200

    Jeg skal skide nu og have mig en shake

--- distanceMeasuring.py
+++ distanceMeasuring.py
@@ -4,50 +4,28 @@ import matplotlib.pyplot as plt
 import xlrd
 import os
 
-#df = pd.read_csv('C:/Users/Morten/Documents/pipsvans/femfyrre/femfyrre_32.csv', header=None, skiprows=2, names=['time','A','B'])
 names = [x[1] for x in os.walk('C:/Users/Morten/Documents/pipsvans')] #liste over subdirs i pipsvans, men den returner også andre ting, så de skal sorteres fra
 subdirs = names[0][1:]
 df = dict.fromkeys(subdirs) # laver et dictionary med keysets fra listen af strings 'subdirs'
 for x in subdirs:
 	df[x] = pd.read_csv('C:/Users/Morten/Documents/pipsvans/'+x+'/'+x+'_32.csv', header=None, skiprows=2, names=['time','A','B']) # mapper keyset til valueset
-
-	#df = pd.read_csv('C:/Users/Morten/Documents/pipsvans/femfyrre/femfyrre_32.csv', header=None, skiprows=2, names=['time','A','B'])
-	#df = pd.read_csv('C:/Users/Morten/Documents/pipsvans/femfyrre/femfyrre_32.csv', header=None, skiprows=2, names=['time','A','B'])
-noise = []
+maxes = []
 for x in subdirs:
-	#noise.append(max(df[x].A[1:500]))
 	if df[x].empty:
-		print(x)
+		0
 	else: 
-		print(max(df[x].A[1:10]))
+		maxes.append(max(df[x].A[1:500]))
+
+noise = float(max(np.asarray(maxes)))
 		
+signalA = df['tyve'].A[df['tyve'].A > noise*2]
 
-#noiseA = max(df.A[1:500]) 
-#noiseB = max(df.B[1:500])
+for x in subdirs:
+	j = df[x].A.values
+	if j.dtype == object:
+		j=np.array(j, dtype=float)	
+	print(j.dtype)
+	j2 = [i for i in j if i > 2.4]
 
-#plt.plot(df.time,df.B)
+print(j2)
 
-print(len(subdirs))
-#signalA = df.A[df.A > noiseA * 3]
-#signalB = df.B[df.B > noiseB * 4]
-#signalAStartt = df.time[signalAstart[0,1]] 
-#signalAStart = signalA.index[0]
-#signalBStart = signalB.index[0]
-#signalATimeStart = df.time[signalAStart]
-#signalBTimeStart = df.time[signalBStart]
-#travelTime = signalATimeStart - signalBTimeStart
-#print(subdirs)
-
-
-#print(travelTime/1000*340)
-#print(signalBTimeStart)
-#print(travelTime)
-#print(signalA.index[0])
-#print(signalATimeStart)
-
-
-#print(os.walk('C:/Users/Morten/Documents/pipsvans'))
-#print(type(signalA))
-
-
-plt.show()
